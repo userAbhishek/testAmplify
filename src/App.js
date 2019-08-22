ae9cdcba67c3fd136dd11f695589fd94b0c83216
Abhishek <devabhishek2014@gmail.com>
2019-08-22 12:59:59 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload part 2.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with AWS
         </a>
       </header>
     </div>
